2c9c6015771c0027cff345ff565b1d515bf15fa4
rahulr41180 <www.rahulr41180@gmail.com>
2022-04-29 20:51:10 +0530

    Update

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 
 // require("dotenv").config()
 
-const MONGODB_URL = process.env.MONGODB_URL
+// const MONGODB_URL = process.env.MONGODB_URL
 
 const Connectdb = () =>
 {
-    // return mongoose.connect("mongodb+srv://rahulr41180:Rahul12345@cluster0.jjbeq.mongodb.net/MongodbLoginSignUpDatabase?retryWrites=true&w=majority")
-    return mongoose.connect(MONGODB_URL);
+    return mongoose.connect("mongodb+srv://rahulr41180:Rahul12345@cluster0.jjbeq.mongodb.net/MongodbLoginSignUpDatabase?retryWrites=true&w=majority")
+    // return mongoose.connect(MONGODB_URL);
 };
 
 module.exports = Connectdb;
